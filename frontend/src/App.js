8bedb24c9a097a10b8c275b85db41dbf80b1adef
phsu40 <phsu40@gatech.edu>
2023-05-16 13:27:18 -0700

    integrate functionality to create new events via New Event From page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
+import NewEventPage, { action as newEventAction } from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootPage from './pages/Root';
 import EventsRootLayout from './pages/EventsRoot';
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ]
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ]
       },
     ]
