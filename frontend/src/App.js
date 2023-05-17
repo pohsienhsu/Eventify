52519a156cc8b38a7a22916cd479a2ac4903d4bc
phsu40 <phsu40@gatech.edu>
2023-05-17 11:54:58 -0700

    Integrate restriction to edit & new event routes for non-login users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as authAction } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
-import { loadToken as tokenLoader } from './util/auth';
+import { tokenLoader, checkAuthTokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
@@ -49,6 +49,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthTokenLoader
               },
             ],
           },
@@ -56,6 +57,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthTokenLoader
           },
         ],
       },
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,8 +1,18 @@
+import {redirect} from 'react-router-dom';
+
 export const getAuthToken = () => {
   const token = localStorage.getItem('token');
   return token;
 }
 
-export const loadToken = () => {
+export const tokenLoader = () => {
   return getAuthToken();
+}
+
+export const checkAuthTokenLoader = () => {
+  const token = tokenLoader();
+  if (!token) {
+    return redirect("/auth");
+  }
+  return null;
 }
