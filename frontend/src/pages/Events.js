2cec3269d8f05ffa2676b2ac585dcdfdb4e2f692
phsu40 <phsu40@gatech.edu>
2023-05-16 20:40:01 -0700

    Implement defer to have loading before events list gets all the data

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,29 +1,38 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
 
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export const loader = async () => {
+const loadEvents = async () => {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // return {isError: true, message: "Could not fetch data"};
     // throw new Response(JSON.stringify({message: 'Could not fetch events.'}), {status: 500})
-    throw json({message: 'Could not fetch events.'}, {status: 500});
+    throw json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export const loader = () => {
+  return defer({
+    events: loadEvents()
+  })
 }
